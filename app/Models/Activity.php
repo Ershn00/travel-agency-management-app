a16ed90fcdab67f83eb18023166e0e1728a937e9
Ershn00 <ee@privilee.ae>
2023-06-28 15:03:25 +0300

    Added Role, Activity and Company models.

---
+++ app/Models/Activity.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+class Activity extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'company_id',
+        'guide_id',
+        'name',
+        'description',
+        'start_date',
+        'end_date',
+        'price',
+        'photo'
+    ];
+
+    public function company(): BelongsTo
+    {
+        return $this->belongsTo(Company::class);
+    }
+
+    public function participants(): BelongsToMany
+    {
+        return $this->belongsToMany(User::class)->withTimestamps();
+    }
+}
