e05fcd5a7d02d59b04a6d17e61f880aa23ea9fbb
Ershn00 <ee@privilee.ae>
2023-06-29 12:55:02 +0300

    Install breeze.

---
+++ database/seeders/RoleSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Role;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class RoleSeeder extends Seeder
+{
+    public function run(): void
+    {
+        Role::create(['name' => 'Administration']);
+        Role::create(['name' => 'Company Owner']);
+        Role::create(['name' => 'Customer']);
+        Role::create(['name' => 'Guide']);
+    }
+}
