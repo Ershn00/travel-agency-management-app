d086816c646a42754af258e4578a6e02966c60f9
Ershn00 <ee@privilee.ae>
2023-07-03 17:22:01 +0300

    Tests for company activity.

---
+++ database/factories/ActivityFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Activity;
+use App\Models\Company;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Carbon;
+
+class ActivityFactory extends Factory
+{
+    public function definition(): array
+    {
+        return [
+            'company_id'  => Company::factory(),
+            'rep_id'      => User::factory()->rep(),
+            'name'        => fake()->name(),
+            'description' => fake()->text(),
+            'start_date'  => Carbon::now(),
+            'price'       => fake()->randomNumber(5),
+        ];
+    }
+}
---
+++ tests/Feature/CompanyActivityTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Activity;
+use App\Models\Company;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Storage;
+use Tests\TestCase;
+
+class CompanyActivityTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_company_owner_can_view_activities_page()
+    {
+        $company = Company::factory()->create();
+        $user = User::factory()->companyOwner()->create(['company_id' => $company->id]);
+
+        $response = $this->actingAs($user)->get(route('companies.activities.index', $company, $company));
+
+        $response->assertOk();
+    }
+
+    public function test_company_owner_can_see_only_his_companies_activities()
+    {
+        $company = Company::factory()->create();
+        $user = User::factory()->companyOwner()->create(['company_id' => $company->id]);
+        $activity = Activity::factory()->create(['company_id' => $company->id]);
+        $activity2 = Activity::factory()->create();
+
+        $response = $this->actingAs($user)->get(route('companies.activities.index', $company));
+
+        $response->assertSeeText($activity->name)
+            ->assertDontSeeText($activity2->name);
+    }
+}
